15848af3d302cc2cb183334cf84dc0d841295e80
Juanpecas <dan1234loaiza@gmail.com>
2024-10-17 13:37:42 +0200

    Actualizacion de package.json para despliegue del backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,19 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: {}, 
+    global: {}, // Esto es útil si alguna librería requiere 'global' en el navegador
   },
+  build: {
+    outDir: 'dist', // Directorio donde se guardarán los archivos de producción
+    sourcemap: true, // Incluye los sourcemaps para facilitar la depuración en producción
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Extrae las dependencias de node_modules en un chunk separado
+          }
+        }
+      }
+    }
+  }
 });
